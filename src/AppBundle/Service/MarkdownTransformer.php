37b6c6b3b94a8f7ebfb2a021c5bf9969322eae18
Randy Binondo <randy.binondo@wrenkitchens.com>
2019-09-26 14:25:41 +0000

    service DI

---
+++ src/AppBundle/Service/MarkdownTransformer.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: clouduser
+ * Date: 9/26/19
+ * Time: 9:59 PM
+ */
+
+namespace AppBundle\Service;
+
+use Doctrine\Common\Cache\Cache;
+use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
+
+class MarkdownTransformer
+{
+    private $markdownParser;
+    /**
+     * @var Cache
+     */
+    private $cache;
+
+    public function __construct(
+        MarkdownParserInterface $markdownParser,
+        Cache $cache
+    )
+    {
+        $this->markdownParser = $markdownParser;
+        $this->cache = $cache;
+    }
+
+    public function parse($str)
+    {
+        $cache = $this->cache;
+        $key = md5($str);
+        if ($this->cache->contains($key)) {
+            return $this->cache->fetch($key);
+        }
+
+        sleep(1);
+        $str = $this->markdownParser->transform($str);
+        $this->cache->save($key, $str);
+
+        return $str;
+    }
+}
